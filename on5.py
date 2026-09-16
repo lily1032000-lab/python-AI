a253792bef092f89b37e31391659f2cf81deeac2
xxx-yyy-zzz <lily1032000@gmail.com>
2026-09-16 13:59:49 +0000

    update on5.py

--- on5.py
+++ on5.py
@@ -1,8 +1,18 @@
-file_box=  ["cmd.exe", "safe_backup.txt", "hack_tool.exe","bypass_system.dat", "script_hack.py"]
+import time
 
-# Tầng 1 (Vòng ngoài): Kiểm tra độ dài của tên file hiện tại phải lớn hơn 8 ký tự.
+cmd_box = ["run_cmd", "admin_power_bypass_1", "user_login", "check_system_hack", "force_stop"]
 
-for current_file in file_box:
-    current_length = len(current_file)
+for current_cmd in cmd_box:
+    current_length = len(current_cmd)
 
-    if
+    if current_length % 2 == 1:
+        current_has_hack = "hack" in current_cmd
+        current_has_system = "system" in current_cmd
+
+        if current_has_hack or current_has_system:
+            start_time = time.perf_counter()
+            current_status = "ok"
+            print(f"{current_status} new : {current_cmd}")
+
+            current_time = time.perf_counter() - start_time
+            print(f"time {current_time:.6f}")      
