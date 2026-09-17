adbde79968a11fc66daa5e980a63f237958c1bef
xxx-yyy-zzz <lily1032000@gmail.com>
2026-09-17 11:36:06 +0000

    update on4.py

--- on4.py
+++ on4.py
@@ -1,31 +1,12 @@
-# giống với on3.py logic nhưng khác ở ngữ cảnh 
-user_box = [
-    "alex",               # Ngắn hơn 5 ký tự -> Bị loại ngay ở Tầng 1
-    "member_2026",        # Dài hơn 5 ký tự, nhưng không có test/guest -> Bị loại ở Tầng 2
-    "guest_account",      # Dài hơn 5 ký tự VÀ có chứa guest -> VƯỢT QUA!
-    "test_user_vip"       # Dài hơn 5 ký tự VÀ có chứa test -> VƯỢT QUA!
-]
+# dùng continue tiết kiệm và tốn ram
+log_box = ["short", "admin_bypass_now", "execute_order_99"]
 
-print("-------bắt đầu lọc-------")
+for current_log in log_box:
+    current_length=len(current_log)
 
-#for /if phải thụt vào cùng một mức code
-for current_user in user_box :
+    if current_length <=10:
+        continue
 
-       # ─── TẦNG 1: KIỂM TRA ĐỘ DÀI ───
-
-    # Đo độ dài của tên người dùng hiện tại
-    current_length=len(current_user)
-
-    if current_length > 5 :
-
-       # Tạo các biến logic
-       current_has_test = "test" in current_user
-       current_has_guest= "guest" in current_user
-
-       # ─── TẦNG 2 (LỒNG BÊN TRONG): KIỂM TRA TỪ KHÓA ───
-       if current_has_test or current_has_guest:
-      # nếu lọc qua nó sẽ thực hiện lệnh ok
-        current_status="ok"
-        print(f"[{current_status}] : [{current_user}]")
-
-print("---hoàn thành---------")
+    if "bypass" in current_log:
+        vaild_log= current_log
+        print(f"[ok] {vaild_log}")
